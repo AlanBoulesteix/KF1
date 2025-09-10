9f7b2fcd2d34e975def94211b9d0c9cbb23aa61c
Leonard FREYDIER <lfreydie@student.42.fr>
2025-09-10 20:46:29 +0200

    feat: \add gdt configuration

--- kernel/includes/numbers.h
+++ kernel/includes/numbers.h
@@ -1,9 +1,13 @@
 #ifndef NUMBERS_H
 #define NUMBERS_H
 
+#include <stdint.h>
+
 int         putnbr(int n);
 int         putnbr_unsigned(unsigned int n);
 int         puthex(unsigned int n, const char *digits);
 int         print_ptr(unsigned long long ptr);
+int         print_bits_32(uint32_t value);
+int         print_bits_64(uint64_t value);
 
 #endif
--- kernel/init/main.c
+++ kernel/init/main.c
@@ -1,7 +1,9 @@
 #include "../includes/tty.h"
+#include "../includes/gdt.h"
 
 void kernel_main(void)
 {
 	terminal_initialize();
 	terminal_writestring("          [red]@@@@@@@ @@@@@@@@@@@[reset]\n        [cyan_light]@@@@@@@@  @@@@@@@@@@@[reset]\n      [blue]@@@@@@@@    @@@  @@@@@@[reset]\n     [green]@@@@@@@          @@@@@@@[reset]\n   [magenta]@@@@@@@          @@@@@@@@ [reset]\n [brown]@@@@@@@           @@@@@@@   [reset]\n [brown_light]@@@@@@@@@@@@@@@@ @@@@@@@  @@[reset]\n [green_light]@@@@@@@@@@@@@@@@ @@@@@@@@@@@[reset]\n [grey_dark]@@@@@@@@@@@@@@@@ @@@@@@@@@@@[reset]\n[white]           @@@@@@            [reset]\n[red_light]            @@@@@@            \n\n\n");
+	dump_gdt();
 }
